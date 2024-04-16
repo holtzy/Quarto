7293b29764e89cfd9c09543661e0bf12e4e93a40
Krzysztof Nowak <64436185+KrzysztofNowakJaw@users.noreply.github.com>
2024-04-16 22:54:24 +0200

    Add files via upload

---
+++ SaveRDS.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(readxl)
+
+df <- read_xlsx("Data/rower.xlsx")
+
+df
+
+df <- df |>
+  complete(Date = seq.Date(
+    from =
+      min(as.Date(Date)), to = max(as.Date(Date)), by = "day"
+  )) |>
+  mutate(Index = row_number(),
+    KilometersHour = (Distance/Duration)*60,
+    across(2:5, ~ifelse(is.na(.), 0, .)),
+         Date = as.Date(Date)
+    )
+
+saveRDS(file = "Biking", object = df)
+
+names(df)
